616b9bfe4a4ead4b99618ab1a42851b0c84f71dd
pranav33317 <113530194+pranav33317@users.noreply.github.com>
2025-05-10 13:47:40 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+class Config:
+    WIDTH = 800
+    HEIGHT = 600
+    FPS = 60
+    GRAVITY = 0.8
+    STICK_GROW_SPEED = 5
+    STICK_FALL_SPEED = 5
+    PLAYER_SPEED = 5
+    PLATFORM_COLOR = (100, 255, 100)
+    PLAYER_COLOR = (0, 0, 255)
+    STICK_COLOR = (255, 0, 0)
+    BG_COLOR = (255, 255, 255)
